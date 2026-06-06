318ad0fbc9f5d87736db60520bc5d897a8ce40a2
rushikeshgund <it@rsraviation.com>
2026-06-06 22:36:10 +0530

    Add Google Analytics 4 (G-37CSSCMZKN)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -69,6 +69,8 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NM4LWTH8');` }} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-37CSSCMZKN" />
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-37CSSCMZKN');` }} />
         <HeadContent />
       </head>
       <body>
